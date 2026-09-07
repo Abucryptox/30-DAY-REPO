20c051118b93abff7750b2582c2e807c394ed85e
Abucryptox <avuvakar011@gmail.com>
2026-09-07 14:48:54 +0100

    day 02

---
+++ Day02/variables1.py
@@ -0,0 +1,12 @@
+# 'day 2:30 days of python programming'
+first_name = "abubakar"
+last_name = "auwal"
+full_name = "abubakar auwal"
+country = "Nigeria"
+city = "gashua"
+age = 19
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+gender, fav_clour = "M", "blue"
---
+++ Day02/variables2.py
@@ -0,0 +1,62 @@
+# 'day 2:30 days of python programming'
+first_name = "abubakar"
+last_name = "auwal"
+full_name = "abubakar auwal"
+country = "Nigeria"
+city = "gashua"
+age = 19
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+gender, fav_clour = "M", "blue"
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(gender))
+print(type(fav_clour))
+print(len(first_name))
+print(len(first_name)> len(last_name))
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+remainder = num_two % num_one
+print(remainder)
+exp = pow(num_one, num_two)
+print(exp)
+floor_division = num_one // num_two
+print(floor_division)
+
+radius = 30
+area_of_circle = 3.142 * radius * radius
+print(f"area = {area_of_circle}")
+circum_of_circle = 2 * 3.142 * radius
+print(f"circumference = {circum_of_circle}")
+radius2 = float(input('enter radius: '))
+area_of_circle = 3.142 * radius2 * radius2
+print(area_of_circle)
+
+
+name = input("enter your name: ")
+second_name = input("enter your second name: ")
+country = input("enter your country: ")
+age = int(input("enter your age: "))
+
+help('keywords')
+
